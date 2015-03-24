9e463e65845dc5d6d60d002162a3ed4e421fa84f
bote795 <nbotell2@gmail.com>
2015-03-24 16:36:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# First Android App
+
+##Runner
+To  Create app used libgdx gdx-setup.jar
+then added the tween engine following : https://github.com/libgdx/libgdx/wiki/Universal-Tween-Engine
+
+##Features
+1. Game
+2. SideScroller
+3. Platformer
+4. Running
+
+##Specifications
---
+++ android/src/com/bote795/runner/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bote795.runner.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bote795.runner.Runner;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Runner(), config);
+	}
+}
